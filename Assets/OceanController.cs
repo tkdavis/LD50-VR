24a4b8d4043a826a4becf54bb5927d3f719d414e
Tyler Davis <tylerkoreydavis@gmail.com>
2022-04-02 17:04:44 -0700

    added ocean mesh generator and materials

--- Assets/OceanController.cs
+++ Assets/OceanController.cs
@@ -1,11 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter))]
 public class OceanController : MonoBehaviour
 {
-    public int dimensions = 10;
+    public int dimensions = 200;
+    public Octave[] Octaves;
+    public float UVScale;
+    public float oceanScale = 4;
+
     Mesh mesh;
     Vector3[] vertices;
     int[] triangles;
@@ -16,6 +19,7 @@ public class OceanController : MonoBehaviour
         GetComponent<MeshFilter>().mesh = mesh;
 
         CreateShape();
+        mesh.uv = GenerateUVs();
         UpdateMesh();
     }
 
@@ -23,29 +27,16 @@ public class OceanController : MonoBehaviour
     {
         vertices = GenerateVerts();
         triangles = GenerateTries();
-        // vertices = new Vector3[]
-        // {
-        //     new Vector3(0,0,0),
-        //     new Vector3(0,0,1),
-        //     new Vector3(1,0,0),
-        //     new Vector3(1,0,1)
-        // };
-
-        // triangles = new int[]
-        // {
-        //     0, 1, 2,
-        //     1, 3, 2
-        // };
     }
 
     Vector3[] GenerateVerts()
     {
-        var verts = new Vector3[(dimensions) * (dimensions)];
+        var verts = new Vector3[(dimensions + 1) * (dimensions + 1)];
         for (int x = 0; x <= dimensions; x++)
         {
             for (int z = 0; z <= dimensions; z++)
             {
-                verts[index(x, z)] = new Vector3(x, 0, z);
+                verts[index(x, z)] = new Vector3(x * oceanScale, 0, z * oceanScale);
             }
         }
 
@@ -54,10 +45,11 @@ public class OceanController : MonoBehaviour
 
     int[] GenerateTries()
     {
-        var tries = new int[mesh.vertices.Length * 6];
+        var tries = new int[vertices.Length * 6];
         for (int x = 0; x < dimensions; x++)
         {
-            for (int z = 0; z < dimensions; z++) {
+            for (int z = 0; z < dimensions; z++)
+            {
                 tries[index(x, z) * 6 + 0] = index(x, z);
                 tries[index(x, z) * 6 + 1] = index(x + 1, z + 1);
                 tries[index(x, z) * 6 + 2] = index(x + 1, z);
@@ -75,13 +67,64 @@ public class OceanController : MonoBehaviour
         return x * (dimensions + 1) + z;
     }
 
+    Vector2[] GenerateUVs()
+    {
+        var uvs = new Vector2[mesh.vertices.Length];
+
+        for (int x = 0; x <= dimensions; x++)
+        {
+            for (int z = 0; z <= dimensions; z++)
+            {
+                var vec = new Vector2((x / UVScale) % 2, (z / UVScale) % 2);
+                uvs[index(x, z)] = new Vector2(vec.x <= 1 ? vec.x : 2 - vec.x, vec.y <= 1 ? vec.y : 2 - vec.y);
+            }
+        }
+
+        return uvs;
+    }
+
+    void UpdateVerts()
+    {
+        for (int x = 0; x <= dimensions; x++)
+        {
+            for (int z = 0; z <= dimensions; z++)
+            {
+                var y = 0f;
+                for (int o = 0; o < Octaves.Length; o++)
+                {
+                    if (Octaves[o].alternate)
+                    {
+                        var perl = Mathf.PerlinNoise((x * Octaves[o].scale.x) / dimensions, (z * Octaves[o].scale.y) / dimensions) * Mathf.PI * 2f;
+                        y += Mathf.Cos(perl + Octaves[o].speed.magnitude * Time.time) * Octaves[o].height;
+                    }
+                }
+                vertices[index(x, z)] = new Vector3(x * oceanScale, y, z * oceanScale);
+            }
+        }
+    }
+
+    void Update()
+    {
+        UpdateMesh();
+    }
+
     void UpdateMesh()
     {
         mesh.Clear();
 
+        UpdateVerts();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
 
         mesh.RecalculateNormals();
+    }
+
+    [Serializable]
+    public struct Octave
+    {
+        public Vector2 speed;
+        public Vector2 scale;
+        public float height;
+        public bool alternate;
     }
 }
